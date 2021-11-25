80f97e672be7d06ec9f796a6eee03881e12d34e9
Mariana Sanchez <73446466+MarianaSnchez1@users.noreply.github.com>
2021-11-25 11:44:33 -0500

    Add files via upload

--- Proba_pro/tablas.R
+++ Proba_pro/tablas.R
@@ -1,19 +1,19 @@
 #Tablas
 
 #Relacion colesterol-edad
-plot(x = heart$Cholesterol, y = heart$Age,
+plot(x = datos$Cholesterol, y = datos$Age,
      main = "Nivel de colesterol con respecto a la edad",
      xlab = "Colesterol (mm/dl)", ylab = "Edad (años)")
 
 #relacion sexo-colesterol
-boxplot(heart$Cholesterol ~ heart$Sex,
-        xlab = "Genero (F:femenino, M:masculino)",
+boxplot(datos$Cholesterol ~ datos$Sex,
+        xlab = "Genero (0:femenino, 1:masculino)",
         ylab = "Nivel de colesterol (mm/dl)",
         main = "Boxplot género con respecto a los resultados en Colesterol",
         col = c("lightpink", "cyan"))
 
-#Tipos de dolor de pecho
-frecuencia <- freq(heart[3]) 
+#Tipos de dolor de pecho- conseguido en una libreria
+#frecuencia <- freq(datos[3]) 
 #Buscar la frecuencia de cada tipo de dolor de pecho que aparecen en la base de datos 
 
 xhelp <- as.factor(c("TA","NAP","ATA","ASY"))
@@ -24,12 +24,15 @@ prom <- round(yhelp/sum(yhelp)*100)
 #Procentaje de los valores
 ahelp <- paste(xhelp,prom)
 phelp <- paste(ahelp,"%",sep="")
-#Etiqueta con el tipo de dolo de pecho y sus porcentajes
+#Etiqueta con el tipo de dolor de pecho y sus porcentajes
 
 pie(yhelp,labels =phelp, main= "Dolor de pecho y su frecuencia en la población")
 
 #Relacion presion arterial-edad
-plot(x = heart$Age, y = heart$RestingBP, type= "h",
+plot(x = datos$Age, y = datos$RestingBP, type= "h",
      xlab = "Edad (años)", ylab = "Presión arterial (mmHg)",
      main = "Relación presión arterial y edad de la población")
 
+#frecuenca del colesterol
+hist(x=datos$Cholesterol, main= 'Frecuencia colesterol',
+     xlab= 'Colesterol')
